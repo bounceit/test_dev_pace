6c0d003228d9fe48ce56da895ab1fd85ebce7c10
Oleksandr <bounceit1221@gmail.com>
2022-10-18 13:06:47 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,45 +9,74 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(
-        items: List<String>.generate(100, (i) => 'Item $i'),
-      ),
+    return const MaterialApp(
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.items}) : super(key: key);
-  final List<String> items;
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    //Не использовал Expanded потому что..
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: CustomScrollView(center: key, slivers: [
+        body: Column(
+      children: const [MyHomePage()],
+    ));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<String> items = [];
+  int index = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        items.add('Item${index++}');
+        setState(() {});
+      }),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: CustomScrollView(clipBehavior: Clip.hardEdge, slivers: [
           SliverAppBar(
-            titleSpacing: height * 0.4,
+            // pinned: true,
+            // scrolledUnderElevation:
+            //     MediaQuery.of(context).size.height * 0.8,
+//  titleSpacing: height * 0.4,
             // floating: true,
+            // elevation: 0.0,
+            // forceElevated: true,
             backgroundColor: Colors.white,
-            expandedHeight: height * 0.8,
+            expandedHeight: MediaQuery.of(context).size.height * 0.8,
             flexibleSpace: FlexibleSpaceBar(
-              centerTitle: true,
-              title: Padding(
-                padding: const EdgeInsets.fromLTRB(30, 30, 30, 150),
-                child: Image.asset('assets/images/logo.png'),
+              // titlePadding: EdgeInsets.all(30),
+              // centerTitle: true,
+              background: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Image.asset('assets/images/logo.png'),
+                ),
               ),
+              collapseMode: CollapseMode.pin,
             ),
           ),
           SliverGrid(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2,
-              mainAxisSpacing: width * 0.12,
-              crossAxisSpacing: height * 0.07,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
               childAspectRatio: 2.0,
             ),
             delegate: SliverChildBuilderDelegate(
